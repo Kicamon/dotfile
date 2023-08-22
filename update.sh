0be8423ba2dd32368576112af04a0a601592cf09
KicamonIce <1532931259@qq.com>
2023-08-22 21:21:16 +0800

    2023/08/22,21:21

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 time=$(date "+%Y/%m/%d,%H:%M")
 
-git add ./alacritty ./dunst ./fcitx5 ./fish ./i3 ./kitty ./mpd ./ncmpcpp ./picom ./polybar ./rofi ./wallpaper ./zsh ./ranger ./update.sh
+git add ./alacritty ./dunst ./fcitx5 ./fish ./i3 ./kitty ./mpd ./ncmpcpp ./picom ./polybar ./rofi ./wallpaper ./zsh ./ranger ./neofetch ./update.sh
 git commit -m ${time}
 git push
