567406bbaae8a8bcc2286477b09e45e46939323a
KicamonIce <1532931259@qq.com>
2023-09-26 16:38:09 +0800

    2023/09/26,16:38

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 time=$(date "+%Y/%m/%d,%H:%M")
 
-git add ./alacritty ./dunst ./fcitx5 ./fish ./i3 ./kitty ./mpd ./ncmpcpp ./picom ./polybar ./rofi ./wallpaper ./zsh ./ranger ./neofetch ./update.sh ./tmux ./st ./starship.toml ./dwm
+git add ./alacritty ./dunst ./fcitx5 ./fish ./i3 ./kitty ./mpd ./ncmpcpp ./picom ./polybar ./rofi ./wallpaper ./zsh ./ranger ./neofetch ./update.sh ./tmux ./st ./starship.toml
 wait
 git commit -m ${time}
 wait
