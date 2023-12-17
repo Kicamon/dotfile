d101c63e4ae7e7aa102718e7105754f66c97dbdd
KicamonIce <1532931259@qq.com>
2023-12-17 21:09:48 +0800

    2023/12/17,21:09

--- wallpaper/Rchange.sh
+++ wallpaper/Rchange.sh
@@ -8,7 +8,7 @@ index=`head --lines=1 ~/.config/wallpaper/index.txt`
 while true
 do
   index=`expr ${RANDOM} % ${len}`
-	feh --bg-fill ${Path}${files[$index]}
+  feh --bg-fill ${Path}${files[$index]}
   echo $index > ~/.config/wallpaper/index.txt
   sleep 300
 done
--- wallpaper/wallpaper.sh
+++ wallpaper/wallpaper.sh
@@ -9,23 +9,23 @@ index=`head --lines=1 ~/.config/wallpaper/index.txt`
 if [ $1 == 0 ]
 then
   index=0
-	feh --bg-fill ${Path}${files[$index]}
+  feh --bg-fill ${Path}${files[$index]}
 elif [ $1 == 1 ]
 then
-	index=`expr ${index} - 1`
-	if [ ${index} -lt 0 ]
-	then
+  index=`expr ${index} - 1`
+  if [ ${index} -lt 0 ]
+  then
     index=`expr ${len} - 1`
-	fi
-	feh --bg-fill ${Path}${files[$index]}
+  fi
+  feh --bg-fill ${Path}${files[$index]}
 elif [ $1 == 2 ]
 then
-	index=`expr ${index} + 1`
-	if [ ${index} -ge ${len} ]
-	then
+  index=`expr ${index} + 1`
+  if [ ${index} -ge ${len} ]
+  then
     index=0
-	fi
-	feh --bg-fill ${Path}${files[${index}]}
+  fi
+  feh --bg-fill ${Path}${files[${index}]}
 elif [ $1 == 3 ]
 then
   RCHANGE=$(ps -ef | grep Rchange.sh | grep -v grep)
