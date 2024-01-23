e6690f3c9b09f4d4e4f72c1cdae144e754760478
KicamonIce <1532931259@qq.com>
2024-01-23 17:34:29 +0800

    2024/01/23,17:34

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 # /usr/bin/env bash
 
+# 基础
+sudo pacman -S yay python clang feh
+
 # 编程
-sudo pacman -S python clang nvim python-pynvim lua-language-server jedi-language-server nodejs yarn bash-language-server vscode-json-languageserver vim-language-server python-black fd ripgrep
+sudo pacman -S nvim python-pynvim lua-language-server jedi-language-server nodejs yarn bash-language-server vscode-json-languageserver vim-language-server python-black fd ripgrep
