ddf22408253eee687ae3d574da5deaf1aba3f3c2
KicamonIce <1532931259@qq.com>
2024-05-06 13:37:44 +0800

    2024/05/06,13:37

--- ranger/commands.py
+++ ranger/commands.py
@@ -63,112 +63,113 @@ class my_edit(Command):
         return self._tab_directory_content()
 
 
-class fzf_select(Command):
-    """
-    :fzf_select
-
-    Find a file using fzf.
-
-    With a prefix argument select only directories.
-
-    See: https://github.com/junegunn/fzf
-    """
-
-    def execute(self):
-        import subprocess
-        import os.path
-
-        if self.quantifier:
-            # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-
-        else:
-            # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-
-        fzf = self.fm.execute_command(
-            command, universal_newlines=True, stdout=subprocess.PIPE
-        )
-        stdout, stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.rstrip("\n"))
-            if os.path.isdir(fzf_file):
-                self.fm.cd(fzf_file)
-            else:
-                self.fm.select_file(fzf_file)
-
-
 # class fzf_select(Command):
 #     """
 #     :fzf_select
+#
 #     Find a file using fzf.
-#     With a prefix argument to select only directories.
+#
+#     With a prefix argument select only directories.
 #
 #     See: https://github.com/junegunn/fzf
 #     """
 #
 #     def execute(self):
 #         import subprocess
-#         import os
-#         from ranger.ext.get_executables import get_executables
+#         import os.path
 #
-#         if "fzf" not in get_executables():
-#             self.fm.notify("Could not find fzf in the PATH.", bad=True)
-#             return
+#         if self.quantifier:
+#             # match only directories
+#             command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+#             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
 #
-#         fd = None
-#         if "fdfind" in get_executables():
-#             fd = "fdfind"
-#         elif "fd" in get_executables():
-#             fd = "fd"
-#
-#         if fd is not None:
-#             hidden = "--hidden" if self.fm.settings.show_hidden else ""
-#             exclude = "--no-ignore-vcs --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.local,.steam,.m2,.rangerdir,.ssh,.ghidra,.mozilla} --exclude '*.py[co]' --exclude '__pycache__'"
-#             only_directories = "--type directory" if self.quantifier else ""
-#             fzf_default_command = "{} --follow {} {} {} --color=always".format(
-#                 fd, hidden, exclude, only_directories
-#             )
 #         else:
-#             hidden = (
-#                 "-false" if self.fm.settings.show_hidden else r"-path '*/\.*' -prune"
-#             )
-#             exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
-#             only_directories = "-type d" if self.quantifier else ""
-#             fzf_default_command = (
-#                 "find -L . -mindepth 1 {} -o {} -o {} -print | cut -b3-".format(
-#                     hidden, exclude, only_directories
-#                 )
-#             )
-#
-#         env = os.environ.copy()
-#         env["FZF_DEFAULT_COMMAND"] = fzf_default_command
-#         env[
-#             "FZF_DEFAULT_OPTS"
-#         ] = '--height=100% --layout=reverse --ansi --preview="{}"'.format(
-#             """
-#            (
-#                ~/Tools/Other/fzf-scope.sh {} ||
-#                #batcat --color=always {} ||
-#                #bat --color=always {} ||
-#                #cat {} ||
-#                tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
-#            ) 2>/dev/null | head -n 100
-#        """
-#         )
+#             # match files and directories
+#             command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+#             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
 #
 #         fzf = self.fm.execute_command(
-#             "fzf --no-multi", env=env, universal_newlines=True, stdout=subprocess.PIPE
+#             command, universal_newlines=True, stdout=subprocess.PIPE
 #         )
-#         stdout, _ = fzf.communicate()
+#         stdout, stderr = fzf.communicate()
 #         if fzf.returncode == 0:
-#             selected = os.path.abspath(stdout.strip())
-#             if os.path.isdir(selected):
-#                 self.fm.cd(selected)
+#             fzf_file = os.path.abspath(stdout.rstrip("\n"))
+#             if os.path.isdir(fzf_file):
+#                 self.fm.cd(fzf_file)
 #             else:
-#                 self.fm.select_file(selected)
+#                 self.fm.select_file(fzf_file)
+#
+
+
+class fzf_select(Command):
+    """
+    :fzf_select
+    Find a file using fzf.
+    With a prefix argument to select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+
+    def execute(self):
+        import subprocess
+        import os
+        from ranger.ext.get_executables import get_executables
+
+        if "fzf" not in get_executables():
+            self.fm.notify("Could not find fzf in the PATH.", bad=True)
+            return
+
+        fd = None
+        if "fdfind" in get_executables():
+            fd = "fdfind"
+        elif "fd" in get_executables():
+            fd = "fd"
+
+        if fd is not None:
+            hidden = "--hidden" if self.fm.settings.show_hidden else ""
+            exclude = "--no-ignore-vcs --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.local,.steam,.m2,.rangerdir,.ssh,.ghidra,.mozilla} --exclude '*.py[co]' --exclude '__pycache__'"
+            only_directories = "--type directory" if self.quantifier else ""
+            fzf_default_command = "{} --follow {} {} {} --color=always".format(
+                fd, hidden, exclude, only_directories
+            )
+        else:
+            hidden = (
+                "-false" if self.fm.settings.show_hidden else r"-path '*/\.*' -prune"
+            )
+            exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
+            only_directories = "-type d" if self.quantifier else ""
+            fzf_default_command = (
+                "find -L . -mindepth 1 {} -o {} -o {} -print | cut -b3-".format(
+                    hidden, exclude, only_directories
+                )
+            )
+
+        env = os.environ.copy()
+        env["FZF_DEFAULT_COMMAND"] = fzf_default_command
+        env["FZF_DEFAULT_OPTS"] = (
+            '--height=100% --layout=reverse --ansi --preview="{}"'.format(
+                """
+           (
+               ~/Tools/Other/fzf-scope.sh {} ||
+               #batcat --color=always {} ||
+               #bat --color=always {} ||
+               #cat {} ||
+               tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
+           ) 2>/dev/null | head -n 100
+       """
+            )
+        )
+
+        fzf = self.fm.execute_command(
+            "fzf --no-multi", env=env, universal_newlines=True, stdout=subprocess.PIPE
+        )
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            selected = os.path.abspath(stdout.strip())
+            if os.path.isdir(selected):
+                self.fm.cd(selected)
+            else:
+                self.fm.select_file(selected)
 
 
 # 解压缩
