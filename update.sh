49d619136b75c047286a611ff9b6e6aa46f29630
Kicamon <1532931259@qq.com>
2024-05-30 09:32:35 +0800

    2024/05/30,09:32

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 time=$(date "+%Y/%m/%d,%H:%M")
 
-cp -r ~/.config/alacritty ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/tmux ~/.config/starship.toml ~/.config/vim ~/.config/install.sh ./
+cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/tmux ~/.config/starship.toml ~/.config/vim ~/.config/install.sh ./
 wait
 git add .
 wait
