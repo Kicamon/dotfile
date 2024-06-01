4abfa315c0c5ada6eb2bc858800402ecc71a7ade
Kicamon <1532931259@qq.com>
2024-06-01 18:53:35 +0800

    xinitrc

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ./
+cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.xinitrc ./
 cp -r ~/.config/tmux/tmux.conf ./tmux/
