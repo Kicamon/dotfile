a2ddb171a2588faea79097deda108095ade2eb9a
Kicamon <1532931259@qq.com>
2024-06-01 18:14:57 +0800

    zsh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-time=$(date "+%Y/%m/%d,%H:%M")
+# time=$(date "+%Y/%m/%d,%H:%M")
 
 cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/tmux ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.config/install.sh ./
-wait
-git add .
-wait
-git commit -m ${time}
-wait
-git push
+# wait
+# git add .
+# wait
+# git commit -m ${time}
+# wait
+# git push
