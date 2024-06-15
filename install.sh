d27394c0daf463bfd5e6a5d217cfcf20dbf54c99
Kicamon <1532931259@qq.com>
2024-06-16 01:02:21 +0800

    wallpaper

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # font
-yay -S adobe-source-code-pro-font adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-otc-fonts nerd-fonts-droid-sans-mono nono-fonts-emoji noto-color-emoji-fontconfig noto-fonts noto-fonts-sc opendesktop-fonts otf-codenewroman-nerd ttf-arphic-ukai ttf-arphic-uming ttf-carlito ttf-dejavu ttf-droid ttf-firacode-nerd ttf-foundertype-sc-fonts ttf-hack ttf-hannom ttf-inconsolata ttf-iosevka-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-joypixels ttf-liberation ttf-linux-libertine ttf-maple ttf-maple-nerd ttf-maple-sc-nerd ttf-ms-win11-auto-zh_cn ttf-opensans ttf-sourcecodepro-nerd ttf-twemoji-color ttf-wps-fonts wqy-bitmapfont wqy-microhei wqy-zenhei
+yay -S adobe-source-code-pro-font adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-otc-fonts nono-fonts-emoji noto-color-emoji-fontconfig noto-fonts noto-fonts-sc opendesktop-fonts otf-droid-nerd otf-codenewroman-nerd ttf-arphic-ukai ttf-arphic-uming ttf-carlito ttf-dejavu ttf-droid ttf-firacode-nerd ttf-foundertype-sc-fonts ttf-hack ttf-hannom ttf-inconsolata ttf-iosevka-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-joypixels ttf-liberation ttf-linux-libertine ttf-maple ttf-maple-nerd ttf-maple-sc-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-opensans ttf-sourcecodepro-nerd ttf-twemoji-color ttf-wps-fonts wqy-bitmapfont wqy-microhei wqy-zenhei
 
 # code
 sudo pacman -S clang nodejs npm yarn lua python python-black neovim python-pynvim bash-language-server pyright fd lua-language-server ripgrep typescript-language-server vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver vim-language-server
