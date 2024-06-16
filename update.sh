483015d4580caa0d5630bfa7a866ba522886907d
Kicamon <1532931259@qq.com>
2024-06-16 11:32:23 +0800

    fonts

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 
 cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.xinitrc ./
 cp -r ~/.config/tmux/tmux.conf ./tmux/
+cp -r ~/.local/share/fonts ./
