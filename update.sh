2950bc9c753b648b73e33feb45e9c1f253a5f2ae
Kicamon <1532931259@qq.com>
2024-06-18 01:06:25 +0800

    up

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cp -r ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.xinitrc ./
-cp -r ~/.config/tmux/tmux.conf ./tmux/
-cp -r ~/.local/share/fonts ./
+cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.config/tmux ./config
+cp  ~/.xinitrc ~/.Xresources ./
+cp -rp ~/.local/share/fonts ~/.local/share/icons ./local
