229588876cfa11bfb86cdcb63b8771aead1410d2
Kicamon <1532931259@qq.com>
2024-06-21 13:51:33 +0800

    new wallpapers

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.config/tmux ./config
+cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ./config
+cp -rp  ~/.config/tmux/tmux.conf ./config/tmux
 cp  ~/.xinitrc ~/.Xresources ~/.npmrc ./
 cp -rp ~/.local/share/fonts ~/.local/share/icons ./local
+
+echo "update completed"
