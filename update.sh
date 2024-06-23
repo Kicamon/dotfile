3188e8363906cb4ff26b634cc10e0018a10b7ed0
Kicamon <1532931259@qq.com>
2024-06-24 00:26:41 +0800

    add theme and wallpapers

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
+rm -r ./config/wallpaper
 cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/mpd ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ./config
 cp -rp  ~/.config/tmux/tmux.conf ./config/tmux
 cp  ~/.xinitrc ~/.Xresources ~/.npmrc ./
-cp -rp ~/.local/share/fonts ~/.local/share/icons ./local
+cp -rp ~/.local/share/fonts ~/.local/share/icons ~/.local/share/themes ./local
 
-echo "update completed"
+echo -e "\e[31mupdate completed\e[0m"
