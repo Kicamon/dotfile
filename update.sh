b3fdb5127d0644d25ecc1aa9572839078c915f2d
Kicamon <1532931259@qq.com>
2024-06-25 16:44:45 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -r ./config/wallpaper
-cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ./config
+cp -rp ~/.config/alacritty ~/.config/i3 ~/.config/dunst ~/.config/fcitx5 ~/.config/fish ~/.config/kitty ~/.config/ncmpcpp ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/ranger ~/.config/neofetch ~/.config/starship.toml ~/.config/vim ~/.config/yazi ~/.config/fontconfig ~/.config/gtk-* ~/.config/qt6ct ~/.config/Kvantum ./config
 cp ~/.config/tmux/tmux.conf ./config/tmux
 cp ~/.config/mpd/mpd.conf ./config/mpd
 cp  ~/.xinitrc ~/.Xresources ~/.npmrc ./
