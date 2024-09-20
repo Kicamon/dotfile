08b419c7513f3fdfde477b09f318cee30181503c
Kicamon <kicamonice@gmail.com>
2024-09-20 20:29:10 +0800

    update

--- config/zsh/file_preview.sh
+++ config/zsh/file_preview.sh
@@ -6,27 +6,27 @@ category=${mime%%/*}
 # get dir
 dir=$1
 if [[ -z "$dir" ]]; then
-	dir=$2
+  dir=$2
 fi
 dir=$(lua $HOME/.config/zsh/quick_jump.lua $dir)
 dir=$(eval echo "$dir")
 
 # preview
 if [ -d "$dir" ]; then
-	echo -e "\e[1;34m Directory: $dir \e[0m"
-	exa -l --no-user --no-time --icons --no-permissions --no-filesize "$dir" 2>/dev/null
+  echo -e "\e[1;34m Directory: $dir \e[0m"
+  exa -l --no-user --no-time --icons --no-permissions --no-filesize "$dir" 2>/dev/null
 elif [ "$category" = text ]; then
-	(bat -p --style numbers --color=always "$1") 2>/dev/null | head -1000
+  (bat -p --style numbers --color=always "$1") 2>/dev/null | head -1000
 elif [ "$mime" = application/pdf ]; then
-	pdftotext $1 - | less
+  pdftotext $1 - | less
 elif [ "$2" == "-kill" ]; then
-	top_output=$(top -bn1 | grep "$1")
+  top_output=$(top -bn1 | grep "$1")
 
-	CPU_USAGE=$(echo "$top_output" | awk '{sum += $9} END {print sum}')
-	MEM_USAGE=$(echo "$top_output" | awk '{sum += $10} END {print sum}')
+  CPU_USAGE=$(echo "$top_output" | awk '{sum += $9} END {print sum}')
+  MEM_USAGE=$(echo "$top_output" | awk '{sum += $10} END {print sum}')
 
-	echo "$1 进程的CPU使用率: $CPU_USAGE%"
-	echo "$1 进程的内存使用率: $MEM_USAGE%"
+  echo "$1 进程的CPU使用率: $CPU_USAGE%"
+  echo "$1 进程的内存使用率: $MEM_USAGE%"
 else
-	echo $1 is a $category file
+  echo $1 is a $category file
 fi
--- update.sh
+++ update.sh
@@ -1,34 +1,32 @@
 #!/usr/bin/env bash
 
-update() {
-	cp -rp ~/.config/Kvantum \
-		~/.config/dunst \
-		~/.config/fcitx5 \
-		~/.config/fish \
-		~/.config/fontconfig \
-		~/.config/gtk-* \
-		~/.config/i3 \
-		~/.config/kitty \
-		~/.config/neofetch \
-		~/.config/picom \
-		~/.config/polybar \
-		~/.config/qt6ct \
-		~/.config/ranger \
-		~/.config/rofi \
-		~/.config/starship.toml \
-		~/.config/tmux \
-		~/.config/vim \
-		~/.config/yazi \
-		~/.config/zsh \
-		./config
-	cp ~/.xinitrc \
-		~/.Xresources \
-		./user
-	cp -rp ~/.local/share/fcitx5 \
-		~/.local/share/fonts \
-		~/.local/share/themes \
-		./local
-}
+cp -rp ~/.config/Kvantum \
+	~/.config/dunst \
+	~/.config/fcitx5 \
+	~/.config/fish \
+	~/.config/fontconfig \
+	~/.config/gtk-* \
+	~/.config/i3 \
+	~/.config/kitty \
+	~/.config/neofetch \
+	~/.config/picom \
+	~/.config/polybar \
+	~/.config/qt6ct \
+	~/.config/ranger \
+	~/.config/rofi \
+	~/.config/starship.toml \
+	~/.config/tmux \
+	~/.config/vim \
+	~/.config/yazi \
+	~/.config/zsh \
+	./config
+cp ~/.xinitrc \
+	~/.Xresources \
+	./user
+cp -rp ~/.local/share/fcitx5 \
+	~/.local/share/fonts \
+	~/.local/share/themes \
+	./local
 
 shopt -s dotglob
 
@@ -43,12 +41,7 @@ remove_file() {
 	done
 }
 
-remove_dir() {
-	rm -rf ./local/fcitx5/pinyin ./local/icons/hicolor ./config/tmux/plugins
-}
-
-update
+rm -rf ./local/fcitx5/pinyin ./local/fonts/artfont
 remove_file ./local
-remove_dir
 
 echo -e "\e[31mupdate completed\e[0m"
