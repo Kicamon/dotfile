2d025edcf2a145f007994b2e92ba936609427ab3
Kicamon <kicamonice@gmail.com>
2025-09-30 02:11:26 +0800

    update

--- config/zsh/file_preview.sh
+++ config/zsh/file_preview.sh
@@ -3,30 +3,52 @@
 mime=$(file -bL --mime-type "$1")
 category=${mime%%/*}
 
-# get dir
+# ============get dir=====================
+FILE_PATH="${HOME}/.local/state/zsh_jump_list"
+declare -A jump
+if [[ ! -f "$FILE_PATH" ]]; then
+	touch "$FILE_PATH"
+	return
+fi
+
+while IFS=' ' read -r keyword dir; do
+	if [[ -n "$keyword" && -n "$dir" ]]; then
+		jump["$keyword"]="$dir"
+	fi
+done <"$FILE_PATH"
+
+change_dir() {
+  local opt="$1"
+	if [[ -n "${jump[$opt]}" ]]; then
+		echo "${jump[$opt]}"
+	else
+		echo "$opt"
+	fi
+}
+
 dir=$1
 if [[ -z "$dir" ]]; then
-  dir=$2
+	dir=$2
 fi
-dir=$(lua $HOME/.config/zsh/utils/quick_jump.lua $dir)
+dir=$(change_dir "$dir")
 dir=$(eval echo "$dir")
 
-# preview
+# =======================previe==========================
 if [ -d "$dir" ]; then
-  echo -e "\e[1;34m Directory: $dir \e[0m"
-  exa -l --no-user --no-time --icons --no-permissions --no-filesize "$dir" 2>/dev/null
+	echo -e "\e[1;34m Directory: $dir \e[0m"
+	exa -l --no-user --no-time --icons --no-permissions --no-filesize "$dir" 2>/dev/null
 elif [ "$category" = text ]; then
-  (bat -p --style numbers --color=always "$1") 2>/dev/null | head -1000
+	(bat -p --style numbers --color=always "$1") 2>/dev/null | head -1000
 elif [ "$mime" = application/pdf ]; then
-  pdftotext $1 - | less
+	pdftotext $1 - | less
 elif [ "$2" == "-kill" ]; then
-  top_output=$(top -bn1 | grep "$1")
+	top_output=$(top -bn1 | grep "$1")
 
-  CPU_USAGE=$(echo "$top_output" | awk '{sum += $9} END {print sum}')
-  MEM_USAGE=$(echo "$top_output" | awk '{sum += $10} END {print sum}')
+	CPU_USAGE=$(echo "$top_output" | awk '{sum += $9} END {print sum}')
+	MEM_USAGE=$(echo "$top_output" | awk '{sum += $10} END {print sum}')
 
-  echo "$1 进程的CPU使用率: $CPU_USAGE%"
-  echo "$1 进程的内存使用率: $MEM_USAGE%"
+	echo "$1 进程的CPU使用率: $CPU_USAGE%"
+	echo "$1 进程的内存使用率: $MEM_USAGE%"
 else
-  echo $1 is a $category file
+	echo $1 is a $category file
 fi
--- config/zsh/utils/gd.zsh
+++ config/zsh/utils/gd.zsh
@@ -1,28 +1,60 @@
-qj_path=${HOME}'/.config/zsh/utils/quick_jump.sh'
+FILE_PATH="${HOME}/.local/state/zsh_jump_list"
+
+typeset -A jump
+
+read_jump_list() {
+	if [[ ! -f "$FILE_PATH" ]]; then
+		touch "$FILE_PATH"
+		return
+	fi
+
+	while IFS=' ' read -r keyword dir; do
+		if [[ -n "$keyword" && -n "$dir" ]]; then
+			jump["$keyword"]="$dir"
+		fi
+	done <"$FILE_PATH"
+}
+
+add_jump_list() {
+	local keyword="$1"
+	local dir="$2"
+	jump["$keyword"]="$dir"
+	echo "$keyword $dir" >>"$FILE_PATH"
+}
+
+change_dir() {
+	if [[ ! -n "$1" ]]; then
+		echo "~"
+	elif [[ -n "${jump[\"$1\"]}" ]]; then
+		echo "${jump[\"$1\"]}"
+	else
+		echo "$1"
+	fi
+}
+
+list_jump() {
+  for keyword in ${(k)jump[@]}; do
+    echo ${(Q)keyword}
+	done
+}
+
+read_jump_list
 
 gd() {
 	local opt="$1"
 
 	if [[ "$opt" == "-a" ]]; then
-		bash ${qj_path} -a $2 $(pwd)
+		add_jump_list $2 $(pwd)
 	elif [[ "$opt" == "-l" ]]; then
-		bash ${qj_path} -l
+		list_jump
 	elif [[ "$opt" == "-e" ]]; then
-		$EDITOR "/home/KicamonIce/.local/state/zsh_jump_list"
+		$EDITOR "$FILE_PATH"
 	else
 		local dir=$opt
 		if [[ ! -d "$dir" ]]; then
-			dir=$(bash ${qj_path} $dir)
+			dir=$(change_dir $dir)
 		fi
 		dir=$(eval echo "$dir")
 		cd "$dir"
 	fi
 }
-
-_gd_dir_list() {
-	local gd_cmp=($(bash ${qj_path} -l))
-	compadd "$@" -- $gd_cmp
-	_cd "$@" || compadd "$@" -- $gd_cmp
-}
-
-compdef _gd_dir_list gd
