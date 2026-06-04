f6994091ad7a58e09d5fc63f278d8bee6364ad1c
Kicamon <kicamonice@gmail.com>
2026-06-04 11:48:26 +0800

    2026.06.04 11:48:26

--- config/zsh/utils/mocp.sh
+++
@@ -1,6 +0,0 @@
-have_music=$(find . -maxdepth 1 -type f -name '*.mp3' -o -name '*.flac' -o -name '*.wav' | wc -l)
-if [[ ${have_music} -ne 0 ]]; then
-  /usr/bin/mocp .
-else
-  /usr/bin/mocp ~/Music
-fi
